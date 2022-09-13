4d8ebb8fbb8990e6f2bc2af4c1ae08afacc77c51
Isaiah Freeman <104329626+KingTechnician@users.noreply.github.com>
2022-09-12 20:50:58 -0400

    Initial Code (#1)
    The initial code for the MyJournal app.

---
+++ OpenFileButton.java
@@ -0,0 +1,113 @@
+package dataStructures;
+import javafx.event.*;
+import javafx.geometry.*;
+import javafx.stage.FileChooser;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+import javafx.scene.control.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Optional;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+import javax.swing.filechooser.FileNameExtensionFilter;
+public class OpenFileButton 
+{
+	public static EventHandler<ActionEvent> get(boolean authenticated,FileChooser fileChooser,Stage journalStage,TextArea journalEntry,FileChooser.ExtensionFilter fileFilter)
+	{
+		EventHandler<ActionEvent> openFileHandler = new EventHandler<ActionEvent>()
+		{
+			public void handle(ActionEvent event)
+			{
+				if(authenticated)
+				{
+					File openFile = fileChooser.showOpenDialog(journalStage);
+					while(openFile!=null&&!openFile.getName().endsWith(".mjor"))
+					{
+						openFile=  fileChooser.showOpenDialog(journalStage);
+						if(openFile==null)
+						{
+							break;
+						}
+					}
+					
+					String fileContent = "";
+					if(openFile!=null)
+					{
+						TextInputDialog keyDialog = new TextInputDialog("Enter the key for decrypting this file.");
+						keyDialog.setHeaderText("Enter the specified key here");
+						Optional<String> key = keyDialog.showAndWait();
+						String getKey = key.get();
+						HeavyEncryption.AES.secretKey = getKey;
+						HeavyEncryption.AES.salt = "1234";
+						try
+						{
+							int count = 0;
+							Scanner readFile = new Scanner(openFile);
+							while(readFile.hasNextLine())
+							{
+								readFile.nextLine();
+								count++;
+							}
+							readFile.close();
+							readFile = new Scanner(openFile);
+							ArrayList<String> getBinaries = new ArrayList<String>();
+							for (int i = 0; i<count;i++)
+							{
+								String newBinary = "";
+								newBinary = HeavyEncryption.AES.decrypt(readFile.nextLine());
+								getBinaries.add(newBinary);
+								System.out.println(newBinary);
+							}
+							readFile.close();
+							int keyCount = 0;
+							String keyfromFile = "";
+							for (String binary:getBinaries)
+							{
+								if(keyCount<=getKey.length())
+								{
+									keyfromFile+= HeavyEncryption.convertToString(binary);
+								}
+								else
+								{
+									fileContent+=HeavyEncryption.convertToString(binary);
+								}
+								keyCount++;
+							}
+							getKey = getKey.replaceAll("\\P{Print}","");
+							keyfromFile = keyfromFile.replaceAll("\\P{Print}","");
+							
+							if(getKey.equals(keyfromFile))
+							{
+								TextArea newInformation = new TextArea(journalEntry.getText()+fileContent);
+								System.out.println("New information: "+newInformation);
+								Stage newStage = new JournalWindow().journalStage(fileChooser,fileFilter,newInformation);
+								newStage.setTitle(openFile.getName());
+								Rectangle2D screen = Screen.getPrimary().getBounds();
+								newStage.setHeight(screen.getHeight());
+								newStage.setWidth(screen.getWidth());
+								newStage.show();
+							}
+							else
+							{
+								JOptionPane.showMessageDialog(null, "Incorrect Key.");
+							}
+						}
+						catch(FileNotFoundException e)
+						{
+							e.printStackTrace();
+						}
+					}
+				}
+				else
+				{
+					JOptionPane.showMessageDialog(null,"Incorrect key");
+				}
+				
+				
+			}
+		};
+		return openFileHandler;
+	}
+}
---
+++ SaveFileButton.java
@@ -0,0 +1,123 @@
+package dataStructures;
+import javafx.event.*;
+import javafx.scene.*;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import javafx.scene.control.*;
+import javafx.scene.control.Alert.AlertType;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Optional;
+
+import javax.swing.JOptionPane;
+public class SaveFileButton 
+{
+	public static EventHandler<ActionEvent> get(TextArea journalEntry, FileChooser fileChooser, Stage journalStage)
+	{
+		EventHandler<ActionEvent> saveFileHandler = new EventHandler<ActionEvent>()
+		{
+			public void handle(ActionEvent event)
+			{
+				int keyDecision = JOptionPane.showConfirmDialog(null,"Use a custom key?","Key Decision",JOptionPane.YES_NO_OPTION);
+				ProgressIndicator encryptProgress = new ProgressIndicator();
+				Scene progressScene = new Scene(encryptProgress,200,200);
+				Stage encryptStage = new Stage();
+				encryptStage.setScene(progressScene);
+				encryptStage.show();
+				if(keyDecision==JOptionPane.YES_OPTION)
+				{
+					TextInputDialog keyDialog = new TextInputDialog("Enter the key you would like to use to encrypt this file.");
+					keyDialog.setHeaderText("Choose a desired key");
+					Optional<String> receiveKey = keyDialog.showAndWait();
+					String getKey = receiveKey.get();
+					HeavyEncryption.AES.secretKey = getKey;
+					getKey+="\n";
+					HeavyEncryption.AES.salt = "1234";
+					String[] setBinaries = HeavyEncryption.formEncryptedBinaries(journalEntry.getText());
+					String[] binaryKeys = HeavyEncryption.formEncryptedBinaries(getKey);
+					String[] getBinaries = new String[setBinaries.length+binaryKeys.length];
+					int arrayCount = 0;
+					for (int i = 0; i<binaryKeys.length;i++)
+					{
+						getBinaries[arrayCount] = binaryKeys[i];
+						arrayCount++;
+					}
+					for (int i = 0; i<setBinaries.length;i++)
+					{
+						getBinaries[arrayCount] = setBinaries[i];
+						arrayCount++;
+					}
+					File saveFile = fileChooser.showSaveDialog(journalStage);
+					try
+					{
+						FileWriter writeFile = new FileWriter(saveFile);
+						for (int i  =0; i<getBinaries.length;i++)
+						{
+							writeFile.append(getBinaries[i]+"\n");
+							System.out.println(getBinaries[i]);
+							encryptProgress.setProgress(i/getBinaries.length);
+							if((i/getBinaries.length)==((getBinaries.length-1)/getBinaries.length))
+							{
+								encryptStage.close();
+							}
+						}
+						writeFile.close();
+						journalStage.setTitle(saveFile.getName());
+					}
+					catch (IOException e)
+					{
+						e.printStackTrace();
+					}
+				}
+				else
+				{
+					String randomCode = new JournalWindow().randomKey();
+					HeavyEncryption.AES.secretKey = randomCode;
+					randomCode+="\n";
+					HeavyEncryption.AES.salt = "1234";
+					String[] setBinaries = HeavyEncryption.formEncryptedBinaries(journalEntry.getText());
+					String[] binaryKeys = HeavyEncryption.formEncryptedBinaries(randomCode);
+					String[] getBinaries = new String[setBinaries.length+binaryKeys.length];
+					int arrayCount = 0;
+					for (int i = 0; i<binaryKeys.length;i++)
+					{
+						getBinaries[arrayCount] = binaryKeys[i];
+						arrayCount++;
+					}
+					for (int i = 0; i<setBinaries.length;i++)
+					{
+						getBinaries[arrayCount] = setBinaries[i];
+						arrayCount++;
+					}
+					File saveFile = fileChooser.showSaveDialog(journalStage);
+					try
+					{
+						FileWriter writeFile = new FileWriter(saveFile);
+						for (int i  =0; i<getBinaries.length;i++)
+						{
+							writeFile.append(getBinaries[i]+"\n");
+							System.out.println(getBinaries[i]);
+							encryptProgress.setProgress(i/getBinaries.length);
+							if((i/getBinaries.length)==((getBinaries.length-1)/getBinaries.length))
+							{
+								encryptStage.close();
+							}
+						}
+						writeFile.close();
+						journalStage.setTitle(saveFile.getName());
+						Alert codeAlert = new Alert(AlertType.INFORMATION);
+						codeAlert.setTitle("Your randomized code");
+						codeAlert.setContentText("Your randomized key is: "+randomCode);
+						codeAlert.showAndWait();
+					}
+					catch (IOException e)
+					{
+						e.printStackTrace();
+					}
+				}
+			}
+		};
+		return saveFileHandler;
+	}
+}
